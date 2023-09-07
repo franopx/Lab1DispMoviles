a623f2cd4f4928e66d8b627147d29505ea368cc8
franopx <franciscogalenorojas@gmail.com>
2023-09-07 09:38:53 -0300

    not uploaded files

---
+++ app/src/main/java/com/example/appdispmoviles/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appdispmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickTest(view: View?)
+    {
+        // What does it do
+    }
+
+}
