8cefd3f560e4ea101a796cf37580930ba8595f1e
Francisco <franciscogalenorojas@gmail.com>
2023-09-15 19:07:33 -0300

    add settings activity and menus

--- app/src/main/java/com/example/appdispmoviles/MainActivity.kt
+++ app/src/main/java/com/example/appdispmoviles/MainActivity.kt
@@ -2,19 +2,20 @@ package com.example.appdispmoviles
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun onClickTest(view: View?)
-    {
-        // What does it do
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     fun onAboutButtonClick(view: View?)
@@ -23,4 +24,24 @@ class MainActivity : AppCompatActivity() {
         startActivity(switchActivityIntent)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intentPreferences = Intent(this, SettingsActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            R.id.action_about -> {
+                val intentPreferences = Intent(this, AboutActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
 }
