8f021814373e2100ae24ffc623e36640dd1adf92
Francisco <franciscogalenorojas@gmail.com>
2023-09-15 21:25:12 -0300

    add splash screen and settings

---
+++ app/src/main/java/com/example/appdispmoviles/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.appdispmoviles
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val value = 1000
+        val delayMillis = value.toLong()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, delayMillis)
+    }
+}
