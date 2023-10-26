5ad372b40a4cfc450193c87c0388e6c1c113d139
laboratorio1 <triallder@gmail.com>
2023-10-26 09:37:33 -0300

    add background music file and solo game events

--- app/src/main/java/com/example/appdispmoviles/SoloGame.kt
+++ app/src/main/java/com/example/appdispmoviles/SoloGame.kt
@@ -1,11 +1,39 @@
 package com.example.appdispmoviles
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 
 class SoloGame : AppCompatActivity() {
+
+    private lateinit var backgroundMusic: MediaPlayer
+    private lateinit var winSound: MediaPlayer
+    private lateinit var failSound: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solo_game)
+
+        backgroundMusic = MediaPlayer.create(this, R.raw.background_music)
+    }
+
+    public fun onClearButtonClick(view: View?)
+    {
+
+    }
+
+    inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean
+        {
+            return false
+        }
     }
 }
