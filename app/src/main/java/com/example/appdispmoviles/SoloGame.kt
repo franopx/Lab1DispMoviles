e6585dd13daa80004016784a048e63bd63af0815
laboratorio1 <triallder@gmail.com>
2023-10-26 08:59:29 -0300

    add solo game activity

---
+++ app/src/main/java/com/example/appdispmoviles/SoloGame.kt
@@ -0,0 +1,11 @@
+package com.example.appdispmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoloGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solo_game)
+    }
+}
