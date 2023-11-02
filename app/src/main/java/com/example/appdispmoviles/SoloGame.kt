ef9ffbc1959a5d87265fc8010b75ba5545c32825
Francisco <franciscogalenorojas@gmail.com>
2023-11-01 22:43:45 -0300

    add game loop

--- app/src/main/java/com/example/appdispmoviles/SoloGame.kt
+++ app/src/main/java/com/example/appdispmoviles/SoloGame.kt
@@ -1,14 +1,24 @@
 package com.example.appdispmoviles
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class SoloGame : AppCompatActivity() {
 
@@ -20,10 +30,17 @@ class SoloGame : AppCompatActivity() {
 
     private lateinit var speedUp: Handler
 
+    private lateinit var sensorManager: SensorManager
+
+    private val gamePrompts = listOf("Shake!", "Touch!", "Fling!")
+    public var currentGame = 0
+    var gameTime = 3000
+    var gameWin = false
+    lateinit var gameText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solo_game)
-
 
         gestureDetector = GestureDetector(this,GestureListener())
         backgroundMusic = MediaPlayer.create(this, R.raw.background_music)
@@ -37,6 +54,25 @@ class SoloGame : AppCompatActivity() {
 
         winSound = MediaPlayer.create(this, R.raw.positive_sound)
         failSound = MediaPlayer.create(this, R.raw.negative_sound)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(
+                SensorListener(),
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST)
+        }
+
+        gameText = findViewById<TextView>(R.id.accelText)
+        currentGame = Random.nextInt() % 2
+        gameText.text = gamePrompts[currentGame]
+        gameText.setTextColor(Color.RED)
+        val delayMillis = gameTime.toLong()
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            nextGame()
+        }, delayMillis)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -74,6 +110,18 @@ class SoloGame : AppCompatActivity() {
     }
 
     inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+            if(currentGame == 1 && !gameWin)
+            {
+                gameText.setTextColor(Color.GREEN)
+                gameWin = true
+                winSound.start()
+                showToast("Well Done! 1")
+            }
+            return super.onSingleTapConfirmed(e)
+        }
+
         override fun onFling(
             e1: MotionEvent?,
             e2: MotionEvent,
@@ -83,11 +131,92 @@ class SoloGame : AppCompatActivity() {
         {
             if(velocityY < -1000 && velocityX > -1000 && velocityX < 1000)
             {
-                showToast("Fling detected!")
+                if(currentGame == 2 && !gameWin)
+                {
+                    gameText.setTextColor(Color.GREEN)
+                    gameWin = true
+                    winSound.start()
+                    showToast("Well Done! 2")
+                }
+
                 return true
             }
 
             return false
         }
+    }
+
+    var gravity = floatArrayOf(0f, 0f, 0f)
+    var linearAcceleration = floatArrayOf(0f, 0f, 0f)
+
+    inner class SensorListener: SensorEventListener {
+
+        override fun onSensorChanged(event: SensorEvent) {
+            // In this example, alpha is calculated as t / (t + dT),
+            // where t is the low-pass filter's time-constant and
+            // dT is the event delivery rate.
+
+            val alpha: Float = 0.8f
+
+            // Isolate the force of gravity with the low-pass filter.
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+            // Remove the gravity contribution with the high-pass filter.
+            linearAcceleration[0] = event.values[0] - gravity[0]
+            linearAcceleration[1] = event.values[1] - gravity[1]
+            linearAcceleration[2] = event.values[2] - gravity[2]
+
+            val threshold = 1f
+            if(linearAcceleration[1] > threshold)
+            {
+                if(currentGame == 0 && !gameWin)
+                {
+                    gameText.setTextColor(Color.GREEN)
+                    winSound.start()
+                    gameWin = true
+                    showToast("Well Done! 3")
+                }
+            }
+        }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            //
+        }
+    }
+
+    // Game
+    private fun nextGame() {
+
+        if(gameWin)
+        {
+            do {
+                currentGame = Random.nextInt() % 2
+            } while (currentGame < 0 || currentGame > 2)
+
+            gameWin = false
+
+            gameText.setTextColor(Color.RED)
+            val delayMillis = gameTime.toLong()
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                nextGame()
+            }, delayMillis)
+        }
+        else
+        {
+            failSound.start()
+            showToast("Try Again!")
+            val value = 1000
+            val delayMillis = value.toLong()
+
+            val handler = Handler(Looper.getMainLooper())
+            handler.postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }, delayMillis)
+        }
+
     }
 }
